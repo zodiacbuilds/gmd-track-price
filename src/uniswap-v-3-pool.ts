29d3939cfed890a76216a674ee315fb0b391943e
DevZodiac <87861770+Zodiacbits@users.noreply.github.com>
2023-09-13 11:29:44 +0800

    Added token.price const for updating by using GMD/WETH uniV3 pool address

--- src/uniswap-v-3-pool.ts
+++ src/uniswap-v-3-pool.ts
@@ -5,6 +5,23 @@ import {
   Swap
 } from "../generated/schema"
 
+const timestamp = event.block.timestamp
+const token = getOrCreateToken('0x0632742C132413Cd47438691D8064Ff9214aC216') // get or create gmd token
+const tokenPrice = getUniswapPriceInUSD('0x0632742C132413Cd47438691D8064Ff9214aC216') // get token price
+token.price = tokenPrice // set token price as attribute
+updateToken(token, timestamp) // finally save token with updated price
+
+export function getUniswapPriceInUSD(tokenAddress: Address, fee: i32): BigDecimal {
+  const quoter = UniswapQuoter.bind(UNI_QUOTER_ADDRESS)
+  const tokenToWeth = quoter.try_quoteExactInputSingle(
+      tokenAddress,
+      WETH_TOKEN_ADDRESS,
+      fee,
+      BIG_INT_1E18,
+      BIG_INT_0
+    )
+}
+
 export function handleSwap(event: SwapEvent): void {
   let entity = new Swap(
     event.transaction.hash.concatI32(event.logIndex.toI32())
