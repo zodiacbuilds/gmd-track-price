61ee9cf2066c5734362c7be3670ba40710a3f172
DevZodiac <87861770+Zodiacbits@users.noreply.github.com>
2023-09-22 10:00:27 +0800

    Added constants to handleswap function.

--- src/uniswap-v-3-pool.ts
+++ src/uniswap-v-3-pool.ts
@@ -5,12 +5,6 @@ import {
   Swap
 } from "../generated/schema"
 
-const timestamp = event.block.timestamp
-const token = getOrCreateToken('0x0632742C132413Cd47438691D8064Ff9214aC216') // get or create gmd token
-const tokenPrice = getUniswapPriceInUSD('0x0632742C132413Cd47438691D8064Ff9214aC216') // get token price
-token.price = tokenPrice // set token price as attribute
-updateToken(token, timestamp) // finally save token with updated price
-
 export function getUniswapPriceInUSD(tokenAddress: token, fee: i32): BigDecimal {
   const quoter = UniswapQuoter.bind(UNI_QUOTER_ADDRESS)
   const tokenToWeth = quoter.try_quoteExactInputSingle(
@@ -26,6 +20,13 @@ export function handleSwap(event: SwapEvent): void {
   let entity = new Swap(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
+  Address.fromString('0x0632742C132413Cd47438691D8064Ff9214aC216');
+  const timestamp = event.block.timestamp
+  const token = getOrCreateToken('0x0632742C132413Cd47438691D8064Ff9214aC216') // get or create gmd token
+  const tokenPrice = getUniswapPriceInUSD('0x0632742C132413Cd47438691D8064Ff9214aC216') // get token price
+  token.price = tokenPrice // set token price as attribute
+  updateToken(token, timestamp) // finally save token with updated price
+
   entity.sender = event.params.sender
   entity.recipient = event.params.recipient
   entity.amount0 = event.params.amount0
